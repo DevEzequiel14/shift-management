498f66de5e4fa30a0e3e73a4ef0f90baab044da7
Ezequiel Chorolque <ezequielchorolque14@gmail.com>
2025-07-28 15:53:52 -0300

    add footer

--- src/app/modules/dashboard/layout/layout.ts
+++ src/app/modules/dashboard/layout/layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Sidebar } from './components/sidebar/sidebar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-layout',
-  imports: [Sidebar],
+  imports: [Sidebar, Footer],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
